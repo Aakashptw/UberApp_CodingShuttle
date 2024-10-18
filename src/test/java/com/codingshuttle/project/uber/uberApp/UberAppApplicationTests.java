b92bd35b699f053e7c7eb5222b62579c114cc094
ndroid <aakashptw8530@gmail.com>
2024-10-19 00:01:55 +0530

    added email sending services

--- src/test/java/com/codingshuttle/project/uber/uberApp/UberAppApplicationTests.java
+++ src/test/java/com/codingshuttle/project/uber/uberApp/UberAppApplicationTests.java
@@ -1,13 +1,37 @@
 package com.codingshuttle.project.uber.uberApp;
 
+import com.codingshuttle.project.uber.uberApp.services.EmailSenderService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class UberAppApplicationTests {
 
+	@Autowired
+	private EmailSenderService emailSenderService;
+
 	@Test
 	void contextLoads() {
+		emailSenderService.sendEmail(
+				"getes96496@chysir.com",
+				"Regarding subject of the email",
+				"The body of my email"
+		);
+	}
+
+	@Test
+	void sendMultipleEmail(){
+		String email[] = {
+				"getes96496@chysir.com",
+				"aakshptw8530@gmail.com",
+				"ptwaakash@gmail.com",
+				"anujptw61@gmail.com",
+				"stalokpatwa@gmail.com",
+				"sakshatetc08.ss@gmail.com"
+		};
+		emailSenderService.sendEmail(email, "Greetings from Ride booking Application made by Aakash!",
+				"Chalti-Ka-Naam-Gaadi App is waiting for you to onboard!");
 	}
 
 }
